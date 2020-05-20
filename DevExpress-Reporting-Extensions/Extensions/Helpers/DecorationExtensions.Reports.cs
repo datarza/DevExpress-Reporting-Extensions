262fa94447c4c392778b0f1fbede8302a16ab92b
Radu Martin <radu@canamsys.com>
2020-05-20 18:00:36 -0400

    fix methods

--- DevExpress-Reporting-Extensions/Extensions/Helpers/DecorationExtensions.Reports.cs
+++ DevExpress-Reporting-Extensions/Extensions/Helpers/DecorationExtensions.Reports.cs
@@ -12,13 +12,5 @@ namespace DevExpressReportingExtensions.Extensions
             return new DetailReportHelper(report, dataMember);
         }
 
-        public static DetailReportHelper AddMasterDetailReport(this XtraReport report,
-            string dataMember,
-            string headerDataMember,
-            string formatString = null)
-        {
-            return new DetailReportHelper(report, dataMember, headerDataMember, formatString);
-        }
-
     }
 }
